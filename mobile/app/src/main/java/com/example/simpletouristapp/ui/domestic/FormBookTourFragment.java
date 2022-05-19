6797871b7a970aeb8993c13cd60f7fd50d1b4bfa
tanluc12345123 <tanlucnguyen055@gmail.com>
2022-05-19 16:23:24 +0700

    setup ui pay successful and failed

--- mobile/app/src/main/java/com/example/simpletouristapp/ui/domestic/FormBookTourFragment.java
+++ mobile/app/src/main/java/com/example/simpletouristapp/ui/domestic/FormBookTourFragment.java
@@ -10,7 +10,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.simpletouristapp.R;
 import com.example.simpletouristapp.databinding.FormBookTourBinding;
 import com.example.simpletouristapp.databinding.NewsFragmentBinding;
 import com.example.simpletouristapp.ui.news.NewsViewModel;
@@ -39,10 +41,30 @@ public class FormBookTourFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         binding.nameTour.setText(tourName);
 
+        binding.btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.amount.getText().toString().equals("")){
+                    binding.amount.setText("1");
+                }else {
+                    binding.amount.setText(Integer.toString((Integer.parseInt(binding.amount.getText().toString())+1)));
+                }
+
+            }
+        });
+        binding.btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!binding.amount.getText().toString().equals("") && !binding.amount.getText().toString().equals("0")){
+                    binding.amount.setText(Integer.toString((Integer.parseInt(binding.amount.getText().toString())-1)));
+                }
+            }
+        });
+
         binding.btnBookTourForm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Navigation.findNavController(view).navigate(R.id.action_nav_book_tour_to_nav_payment);
             }
         });
     }
